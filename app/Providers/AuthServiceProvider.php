05b4fc794d94c748b4fb9e25c132fcbb2bb524f5
Gianpierre Galvez Wizeline <gianpierre.galvez@wizeline.com>
2021-02-16 05:39:19 -0500

    Testing cors

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
@@ -31,6 +32,9 @@ class AuthServiceProvider extends ServiceProvider
             'app-client-logged' => 'Para que los usuarios puedan consumir cuando ya se logearon'
         ]);
 
-        Passport::routes();
+        Route::prefix('api')->group(function () {
+            Passport::routes();
+        });
+        // Passport::routes();
     }
 }
